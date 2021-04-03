be984598dac472ab4b0bb498dc59ad63f6626a25
Christoph Stiller <cstiller@euclideonholographics.com>
2021-04-03 16:48:26 +0200

    Supporting multiple threads

--- src/silverpp.cpp
+++ src/silverpp.cpp
@@ -31,7 +31,38 @@ struct SPerfEval
   }
 };
 
-void EvaluateSession(_Out_ std::vector<SPerfEval> &evaluation, _In_ CComPtr<IDiaDataSource> &pdbSource, _Inout_ std::deque<size_t> &positions);
+struct SModuleInfo
+{
+  size_t baseAddress;
+  size_t endAddress;
+};
+
+struct SThreadRip
+{
+  DWORD threadId;
+  HANDLE handle;
+  size_t lastRip;
+};
+
+struct SProfileResult
+{
+  std::deque<size_t> directHits;
+};
+
+struct SEvalResult
+{
+  std::vector<SPerfEval> eval;
+};
+
+struct SAppInfo
+{
+  std::vector<SThreadRip> threads;
+  SModuleInfo modules;
+};
+
+SProfileResult ProfileApplication(SAppInfo &appInfo);
+void UpdateAppInfo(SAppInfo &appInfo, const DEBUG_EVENT &evnt);
+SEvalResult EvaluateSession(_In_ CComPtr<IDiaDataSource> &pdbSource, _Inout_ SProfileResult &perfSession);
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -74,16 +105,20 @@ int32_t main(void)
     FATAL_IF(!CreateProcessW(appPath, L"", NULL, NULL, FALSE, DEBUG_PROCESS | CREATE_NEW_CONSOLE, NULL, workingDirectory, &startupInfo, &processInfo), "Unable to start process. Aborting.");
   }
 
-  DEBUG_EVENT debugEvent;
-
-  FATAL_IF(!WaitForDebugEvent(&debugEvent, 1000), "Failed to debug process. Aborting.");
-
-  do
+  SAppInfo appInfo;
+  
+  // Start Debugging. (Apparently required for `EnumProcessModules` to work)
   {
-    FATAL_IF(!ContinueDebugEvent(debugEvent.dwProcessId, debugEvent.dwThreadId, DBG_CONTINUE), "Failed to continue debugged process. Aborting.");
-  } while (WaitForDebugEvent(&debugEvent, 1));
+    DEBUG_EVENT debugEvent;
 
-  size_t moduleVirtualBaseAddress = 0;
+    FATAL_IF(!WaitForDebugEvent(&debugEvent, 1000), "Failed to debug process. Aborting.");
+
+    do
+    {
+      UpdateAppInfo(appInfo, debugEvent);
+      FATAL_IF(!ContinueDebugEvent(debugEvent.dwProcessId, debugEvent.dwThreadId, DBG_CONTINUE), "Failed to continue debugged process. Aborting.");
+    } while (WaitForDebugEvent(&debugEvent, 1));
+  }
 
   // Get Base Address of Main Module.
   {
@@ -99,90 +134,29 @@ int32_t main(void)
     IMAGE_NT_HEADERS ntHeader;
     FATAL_IF(!ReadProcessMemory(processInfo.hProcess, pBaseAddress + moduleHeader.e_lfanew, &ntHeader, sizeof(ntHeader), &bytesRead) || bytesRead != sizeof(ntHeader), "Failed to Read Module NT Header. Aborting.");
 
-    moduleVirtualBaseAddress = (size_t)pBaseAddress + ntHeader.OptionalHeader.BaseOfCode;
+    appInfo.modules.baseAddress = (size_t)pBaseAddress;
+    appInfo.modules.endAddress = appInfo.modules.baseAddress + (size_t)ntHeader.OptionalHeader.SizeOfCode;
   }
-
-  size_t lastRip = 0;
-  std::deque<size_t> positions;
 
   puts("Starting Profiling Loop...");
 
-  CONTEXT threadContext;
-  threadContext.ContextFlags = CONTEXT_CONTROL;
+  SProfileResult profileSession = ProfileApplication(appInfo);
 
-  // Profile Stuff.
-  while (true)
-  {
-    const bool hasDebugEvent = WaitForDebugEvent(&debugEvent, 0);
-
-    if (hasDebugEvent)
-    {
-      if (debugEvent.dwDebugEventCode == EXIT_PROCESS_DEBUG_EVENT)
-        break;
-
-      if (debugEvent.dwThreadId != processInfo.dwThreadId)
-      {
-        if (!ContinueDebugEvent(debugEvent.dwProcessId, debugEvent.dwThreadId, DBG_CONTINUE))
-        {
-          puts("Failed to Continue Application. Stopping the Profiler.");
-          break;
-        }
-
-        SuspendThread(processInfo.hThread);
-      }
-    }
-
-    if (GetThreadContext(processInfo.hThread, &threadContext))
-    {
-      if (threadContext.Rip != lastRip)
-      {
-        positions.emplace_back(threadContext.Rip - moduleVirtualBaseAddress);
-        lastRip = threadContext.Rip;
-      }
-    }
-
-    if (hasDebugEvent)
-    {
-      if (debugEvent.dwThreadId == processInfo.dwThreadId)
-      {
-        if (!ContinueDebugEvent(debugEvent.dwProcessId, debugEvent.dwThreadId, DBG_CONTINUE))
-        {
-          puts("Failed to Continue Application. Stopping the Profiler.");
-          break;
-        }
-      }
-      else
-      {
-        ResumeThread(processInfo.hThread);
-      }
-    }
-    else
-    {
-      YieldProcessor();
-    }
-  }
-
-  printf("Profiler Stopped.\nCaptured %" PRIu64 " positions on thread %" PRIu32 ".\n", positions.size(), processInfo.dwThreadId);
-
-  puts("Sorting Profiling Data...");
-
-  std::sort(positions.begin(), positions.end());
-
+  printf("Profiler Stopped.\nCaptured %" PRIu64 " direct hits.\n", profileSession.directHits.size());
+  
   puts("Evaluating Profiling Data...");
 
-  std::vector<SPerfEval> evaluation;
-
-  EvaluateSession(evaluation, pdbSource, positions);
+  SEvalResult evaluation = EvaluateSession(pdbSource, profileSession);
 
   puts("Sorting Evaluation...");
 
-  std::sort(evaluation.begin(), evaluation.end());
+  std::sort(evaluation.eval.begin(), evaluation.eval.end());
 
   puts("\n\nResults:\n");
 
   size_t count = 0;
 
-  for (const auto &_func : evaluation)
+  for (const auto &_func : evaluation.eval)
   {
     ++count;
 
@@ -197,7 +171,126 @@ int32_t main(void)
 
 ////////////////////////////////////////////////////////////////////////////////
 
-DWORD EvaluateSymbol(CComPtr<IDiaSymbol> &symbol, _Out_ std::vector<SPerfEval> &evaluation, _Inout_ std::deque<size_t> &positions)
+SProfileResult ProfileApplication(SAppInfo &appInfo)
+{
+  SProfileResult ret;
+
+  DEBUG_EVENT debugEvent;
+
+  CONTEXT threadContext;
+  threadContext.ContextFlags = CONTEXT_CONTROL;
+
+  while (true)
+  {
+    const bool hasDebugEvent = WaitForDebugEvent(&debugEvent, 0);
+
+    if (hasDebugEvent)
+    {
+      switch (debugEvent.dwDebugEventCode)
+      {
+      case EXIT_PROCESS_DEBUG_EVENT:
+      {
+        ContinueDebugEvent(debugEvent.dwProcessId, debugEvent.dwThreadId, DBG_CONTINUE);
+        return ret;
+      }
+
+      default:
+      {
+        UpdateAppInfo(appInfo, debugEvent);
+        break;
+      }
+      }
+    }
+
+    for (auto &_thread : appInfo.threads)
+      if (!hasDebugEvent || debugEvent.dwThreadId != _thread.threadId)
+        SuspendThread(_thread.handle);
+
+    for (auto &_thread : appInfo.threads)
+    {
+      if (GetThreadContext(_thread.handle, &threadContext))
+      {
+        if (threadContext.Rip != _thread.lastRip)
+        {
+          const size_t relativeAddress = threadContext.Rip - appInfo.modules.baseAddress;
+
+          if (relativeAddress < appInfo.modules.endAddress)
+            ret.directHits.emplace_back(relativeAddress);
+
+          _thread.lastRip = threadContext.Rip;
+        }
+      }
+    }
+
+    for (auto &_thread : appInfo.threads)
+      if (!hasDebugEvent || debugEvent.dwThreadId != _thread.threadId)
+        ResumeThread(_thread.handle);
+
+    if (hasDebugEvent)
+    {
+      if (!ContinueDebugEvent(debugEvent.dwProcessId, debugEvent.dwThreadId, DBG_CONTINUE))
+      {
+        puts("Failed to Continue Application. Stopping the Profiler.");
+        break;
+      }
+    }
+    else
+    {
+      YieldProcessor();
+    }
+  }
+
+  return ret;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void UpdateAppInfo(SAppInfo &appInfo, const DEBUG_EVENT &evnt)
+{
+  switch (evnt.dwDebugEventCode)
+  {
+  case CREATE_THREAD_DEBUG_EVENT:
+  {
+    HANDLE thread = OpenThread(THREAD_GET_CONTEXT | THREAD_QUERY_INFORMATION | THREAD_SUSPEND_RESUME, FALSE, evnt.dwThreadId);
+
+    if (thread == INVALID_HANDLE_VALUE || thread == nullptr)
+      printf("Failed to open Thread %" PRIu32 " with error %" PRIu32 ".\n", evnt.dwThreadId, GetLastError());
+    else
+      appInfo.threads.push_back({ evnt.dwThreadId, thread, 0 });
+
+    break;
+  }
+
+  case EXIT_THREAD_DEBUG_EVENT:
+  {
+    for (size_t i = 0; i < appInfo.threads.size(); i++)
+    {
+      if (evnt.dwThreadId == appInfo.threads[i].threadId)
+      {
+        CloseHandle(appInfo.threads[i].handle);
+        appInfo.threads.erase(appInfo.threads.begin() + i);
+        break;
+      }
+    }
+
+    break;
+  }
+
+  case EXIT_PROCESS_DEBUG_EVENT:
+    FATAL("Unexpected `EXIT_PROCESS_DEBUG_EVENT`. Aborting.");
+    break;
+
+  case LOAD_DLL_DEBUG_EVENT:
+    break;
+
+  case UNLOAD_DLL_DEBUG_EVENT:
+    break;
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+DWORD EvaluateSymbol(_In_ const CComPtr<IDiaSymbol> &symbol, _Inout_ std::vector<SPerfEval> &evaluation, _Inout_ std::deque<size_t> &positions)
 {
   static DWORD lastTag = SymTagNull;
   static DWORD lastVirtualAddress = 0;
@@ -216,7 +309,10 @@ DWORD EvaluateSymbol(CComPtr<IDiaSymbol> &symbol, _Out_ std::vector<SPerfEval> &
   
     memcpy(func.symbolName, symbolName, sizeof(symbolName));
     symbolName[0] = L'\0';
-  
+
+    while (positions.size() > 0 && positions[0] < lastVirtualAddress)
+      positions.pop_front();
+
     while (positions.size() > 0 && positions[0] >= lastVirtualAddress && positions[0] < virtualAddress)
     {
       func.hitsOffset.emplace_back((uint32_t)(positions.front() - lastVirtualAddress));
@@ -252,8 +348,12 @@ DWORD EvaluateSymbol(CComPtr<IDiaSymbol> &symbol, _Out_ std::vector<SPerfEval> &
   return lastVirtualAddress = virtualAddress;
 }
 
-void EvaluateSession(_Out_ std::vector<SPerfEval> &evaluation, _In_ CComPtr<IDiaDataSource> &pdbSource, _Inout_ std::deque<size_t> &positions)
+SEvalResult EvaluateSession(_In_ CComPtr<IDiaDataSource> &pdbSource, _Inout_ SProfileResult &perfSession)
 {
+  SEvalResult ret;
+
+  std::sort(perfSession.directHits.begin(), perfSession.directHits.end());
+
   CComPtr<IDiaSession> session;
   FATAL_IF(FAILED(pdbSource->openSession(&session)), "Failed to Open Session.");
 
@@ -282,7 +382,7 @@ void EvaluateSession(_Out_ std::vector<SPerfEval> &evaluation, _In_ CComPtr<IDia
 
     do
     {
-      lastVirtualAddress = EvaluateSymbol(symbol, evaluation, positions);
+      lastVirtualAddress = EvaluateSymbol(symbol, ret.eval, perfSession.directHits);
 
       symbol = nullptr;
       fetchedSymbolCount = 0;
@@ -298,7 +398,7 @@ void EvaluateSession(_Out_ std::vector<SPerfEval> &evaluation, _In_ CComPtr<IDia
 
     do
     {
-      lastVirtualAddress = EvaluateSymbol(symbol, evaluation, positions);
+      lastVirtualAddress = EvaluateSymbol(symbol, ret.eval, perfSession.directHits);
 
       symbol = nullptr;
       fetchedSymbolCount = 0;
@@ -310,4 +410,6 @@ void EvaluateSession(_Out_ std::vector<SPerfEval> &evaluation, _In_ CComPtr<IDia
 
     FATAL_IF(FAILED(hr), "Failed to retrieve next element.");
   }
+
+  return ret;
 }
